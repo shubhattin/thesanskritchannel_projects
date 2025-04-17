1044dd222fea35d8574b3a3c9aea8b77ad7ec882
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-04-17 23:58:52 +0530

    restore

--- src/routes/(auth)/(projects)/[project_key]/[[first]]/[[second]]/+page.server.ts
+++ src/routes/(auth)/(projects)/[project_key]/[[first]]/[[second]]/+page.server.ts
@@ -12,7 +12,6 @@ const params_schema = z.object({
 
 export const load: PageServerLoad = async (opts) => {
   const { params, isDataRequest } = opts;
-  if (isDataRequest) return;
   const project_key_ = project_keys_enum_schema.safeParse(params.project_key);
   if (!project_key_.success) {
     error(404, `Not found`);
@@ -23,6 +22,9 @@ export const load: PageServerLoad = async (opts) => {
   const { first, second } = params_schema.parse(params);
 
   let text: shloka_list_type | undefined = undefined;
+  let first_name: string | undefined = undefined;
+  let second_name: string | undefined = undefined;
+  let list_count: number | undefined = undefined;
 
   if (levels === 1) {
     if (first || second) {
@@ -30,18 +32,44 @@ export const load: PageServerLoad = async (opts) => {
     }
     text = await get_text_data_func(project_key, []);
   } else if (levels === 2) {
+    if (first && second) {
+      // if the first type is there then these arguments are not needed
+      error(404, `Not found`);
+    }
     if (first) {
       const text_map = await project_info.map_info();
-      text = await get_text_data_func(project_key, [first]);
+      if (!(first >= 1 && first <= text_map.length)) {
+        error(404, `${level_names[1]} Not found`);
+      }
+      first_name = text_map[first - 1].name_dev;
+      if (!isDataRequest) text = await get_text_data_func(project_key, [first]);
+      list_count = text_map.length;
     }
   } else if (levels === 3) {
     if (first) {
+      const text_map = await project_info.map_info();
+      if (!(first >= 1 && first <= text_map.length)) {
+        error(404, `${level_names[2]} Not found`);
+      }
+      first_name = text_map[first - 1].name_dev;
       if (second) {
-        text = await get_text_data_func(project_key, [first, second]);
+        const second_list = text_map[first - 1].list;
+        if (!(second >= 1 && second <= second_list.length)) {
+          error(404, `${level_names[1]} Not found`);
+        }
+        second_name = second_list[second - 1].name_dev;
+        if (!isDataRequest) text = await get_text_data_func(project_key, [first, second]);
+        list_count = second_list.length;
       }
     }
   }
   return {
-    text
+    project_key,
+    first_name,
+    first,
+    second_name,
+    second,
+    text,
+    list_count
   };
 };

a4cb173be8f10c10d9ebedadb7ea69a9ef8ead90
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-05-16 18:46:07 +0530

    cors added

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,10 +8,34 @@ import { createContext } from '~/api/context';
 export const handle_trpc: Handle = createTRPCHandle({ router, createContext });
 
 export const handle: Handle = async ({ event, resolve }) => {
+  const origin = event.request.headers.get('origin');
+  const isAllowedOrigin = !!origin && ALLOWRD_ORIGINS.includes(origin);
+
+  const CORS_ALLOWED_URLS = ['/api/auth', '/api/ext'];
+  const IS_CORS_ALLOWED_URL = CORS_ALLOWED_URLS.some((url) => event.url.pathname.startsWith(url));
+  // Required for CORS to work
+  if (IS_CORS_ALLOWED_URL && event.request.method === 'OPTIONS' && isAllowedOrigin) {
+    return new Response(null, {
+      headers: {
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers':
+          'Content-Type, Authorization, X-Requested-With, X-Captcha-Response',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Origin': origin,
+        Vary: 'Origin'
+      }
+    });
+  }
   if (event.url.pathname.startsWith('/trpc')) {
     return handle_trpc({ event, resolve });
   }
-  return svelteKitHandler({ event, resolve, auth });
+  const res: Response = await svelteKitHandler({ event, resolve, auth });
+  if (IS_CORS_ALLOWED_URL && isAllowedOrigin) {
+    res.headers.append('Access-Control-Allow-Origin', origin);
+    res.headers.append('Access-Control-Allow-Credentials', 'true');
+    res.headers.append('Vary', 'Origin');
+  }
+  return res;
 };
 
 // buffer pollyfill for netlify

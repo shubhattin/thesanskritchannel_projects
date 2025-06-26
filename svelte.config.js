a56eedad301fa0e07cc17e4d77edd0769f335066
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-06-26 13:51:05 +0530

    vercel build mode added

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter_netlify from '@sveltejs/adapter-netlify';
+import adapter_vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,9 +7,15 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter_netlify({
-      edge: true
-    }),
+    adapter:
+      process.env.BUILD_MODE === 'vercel'
+        ? adapter_vercel({
+            runtime: 'edge',
+            regions: ['sin1']
+          })
+        : adapter_netlify({
+            edge: true
+          }),
     alias: {
       '~': 'src',
       '@data': './data'

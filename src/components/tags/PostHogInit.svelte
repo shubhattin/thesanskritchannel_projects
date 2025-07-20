f37a06ae0683a1645a4b92479bdd2edbb054ea04
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-07-20 07:20:36 +0530

    updatye

--- src/components/tags/PostHogInit.svelte
+++ src/components/tags/PostHogInit.svelte
@@ -11,7 +11,7 @@
     ) {
       import('posthog-js').then((posthog) => {
         posthog.default.init(import.meta.env.VITE_POSTHOG_KEY, {
-          api_host: `${import.meta.env.VITE_POSTHOG_URL ?? import.meta.env.VITE_SITE_URL}/ingest`,
+          api_host: `${import.meta.env.VITE_POSTHOG_URL}`,
           person_profiles: 'identified_only',
           ui_host: 'https://us.posthog.com'
         });

dd5b8d7501846414028e055db8b62679fd43746f
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-08-06 09:49:45 +0530

    init

--- src/routes/(auth)/grammar/+page.svelte
+++ src/routes/(auth)/grammar/+page.svelte
@@ -2,38 +2,12 @@
   import Markdown from 'svelte-markdown';
   import { fade } from 'svelte/transition';
   import pretty_ms from 'pretty-ms';
+  import { Switch } from '@skeletonlabs/skeleton-svelte';
+  import { LANGUAGES, MODELS_LIST, MODEL_NAMES, type models_list_type } from './grammer_data';
 
   let langugae = $state('Hindi');
   let shloka = $state('');
 
-  const LANGUAGES = [
-    'Hindi',
-    'English',
-    'Sanskrit',
-    'Telugu',
-    'Kannada',
-    'Marathi',
-    'Tamil',
-    'Malayalam',
-    'Bengali',
-    'Gujarati'
-  ] as const;
-
-  type models_list_type = (typeof MODELS_LIST)[number];
-  const MODELS_LIST = [
-    'gpt-4.1',
-    'gpt-4.1-mini',
-    'gpt-4.1-nano',
-    'gemini-2.0-flash',
-    'gemini-2.5-flash'
-  ] as const;
-  const MODEL_NAMES: Record<models_list_type, string> = {
-    'gpt-4.1': 'GPT-4.1',
-    'gpt-4.1-mini': 'GPT-4.1 Mini',
-    'gpt-4.1-nano': 'GPT-4.1 Nano',
-    'gemini-2.0-flash': 'Gemini 2.0 Flash',
-    'gemini-2.5-flash': 'Gemini 2.5 Flash'
-  } as const;
   let model: models_list_type = $state('gpt-4.1');
 
   let analysis_result = $state('');
@@ -81,39 +55,49 @@
       time_taken = Date.now() - start_time;
     }
   }
+
+  let load_text_source = $state(true);
 </script>
 
 <svelte:head>
   <title>व्याकरणम्</title>
 </svelte:head>
-
 <div class="mt-6 space-y-4">
-  <div class="space-y-3">
-    <label class="flex items-center gap-2">
-      <span class="label-text text-xs font-semibold">Model</span>
-      <select bind:value={model} class="select w-36 px-1 pr-1.5 text-xs">
-        {#each MODELS_LIST as model_key}
-          <option value={model_key}>{MODEL_NAMES[model_key]}</option>
-        {/each}
-      </select>
-    </label>
-    <label class="flex items-center gap-2">
-      <span class="label-text text-sm">Language</span>
-      <select bind:value={langugae} class="select w-28 text-sm">
-        {#each LANGUAGES as lang}
-          <option value={lang}>{lang}</option>
-        {/each}
-      </select>
-    </label>
-    <textarea class="textarea h-52" bind:value={shloka}></textarea>
+  <div class="flex items-center justify-center gap-2">
+    <span>Custom Text</span>
+    <Switch checked={load_text_source} onCheckedChange={(e) => (load_text_source = e.checked)} />
+    <span>Text Source</span>
   </div>
-  <button
-    class="btn preset-filled-primary-200-800 font-semibold"
-    onclick={analyzeShloka}
-    disabled={is_fetching}
-  >
-    Analyze
-  </button>
+  {#if !load_text_source}
+    <div class="space-y-3">
+      <label class="flex items-center gap-2">
+        <span class="label-text text-xs font-semibold">Model</span>
+        <select bind:value={model} class="select w-36 px-1 pr-1.5 text-xs">
+          {#each MODELS_LIST as model_key}
+            <option value={model_key}>{MODEL_NAMES[model_key]}</option>
+          {/each}
+        </select>
+      </label>
+      <label class="flex items-center gap-2">
+        <span class="label-text text-sm">Language</span>
+        <select bind:value={langugae} class="select w-28 text-sm">
+          {#each LANGUAGES as lang}
+            <option value={lang}>{lang}</option>
+          {/each}
+        </select>
+      </label>
+      <textarea class="textarea h-52" bind:value={shloka}></textarea>
+    </div>
+  {/if}
+  {#if !load_text_source}
+    <button
+      class="btn preset-filled-primary-200-800 font-semibold"
+      onclick={analyzeShloka}
+      disabled={is_fetching}
+    >
+      Analyze
+    </button>
+  {/if}
   {#if is_fetching && !analysis_result}
     <div class="rounded=md h-96 placeholder animate-pulse"></div>
   {:else if analysis_result}
---
+++ src/routes/(auth)/grammar/grammer_data.ts
@@ -0,0 +1,28 @@
+export const LANGUAGES = [
+  'Hindi',
+  'English',
+  'Sanskrit',
+  'Telugu',
+  'Kannada',
+  'Marathi',
+  'Tamil',
+  'Malayalam',
+  'Bengali',
+  'Gujarati'
+] as const;
+
+export type models_list_type = (typeof MODELS_LIST)[number];
+export const MODELS_LIST = [
+  'gpt-4.1',
+  'gpt-4.1-mini',
+  'gpt-4.1-nano',
+  'gemini-2.0-flash',
+  'gemini-2.5-flash'
+] as const;
+export const MODEL_NAMES: Record<models_list_type, string> = {
+  'gpt-4.1': 'GPT-4.1',
+  'gpt-4.1-mini': 'GPT-4.1 Mini',
+  'gpt-4.1-nano': 'GPT-4.1 Nano',
+  'gemini-2.0-flash': 'Gemini 2.0 Flash',
+  'gemini-2.5-flash': 'Gemini 2.5 Flash'
+} as const;

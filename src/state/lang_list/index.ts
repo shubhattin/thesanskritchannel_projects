ab8326bb47c21dd8307e54af513cfd1a4f43ecbc
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-09-12 20:17:14 +0530

    unify lang.ts

--- src/state/lang_list/index.ts
+++ src/state/lang_list/index.ts
@@ -1,14 +1,15 @@
 // ALWAYS BE CAREFUL BEFORE CHANGING LANG JSON
 // AS CHNAGE IN PRE-EXISTING LANG IDS WOULD CAUSE DATA MISMATCHs
 
-import { script_list, lang_list } from './langs.json';
+import langs from './langs.json';
+const { script_list, lang_list } = langs;
 
 export const SCRIPT_LIST = Object.keys(script_list);
 export type script_list_type = keyof typeof script_list;
 export const SCRIPT_LIST_IDS = Object.values(script_list);
 
 export const get_script_from_id = (id: number) => {
-  return SCRIPT_LIST[SCRIPT_LIST_IDS.indexOf(id)];
+  return SCRIPT_LIST[SCRIPT_LIST_IDS.indexOf(id)] as script_list_type;
 };
 
 export const LANG_LIST = Object.keys(lang_list);
@@ -17,7 +18,7 @@ export type lang_list_type = keyof typeof lang_list;
 // the langs enum in schema.ts has to be updated manually
 
 export const get_lang_from_id = (id: number) => {
-  return LANG_LIST[LANG_LIST_IDS.indexOf(id)];
+  return LANG_LIST[LANG_LIST_IDS.indexOf(id)] as lang_list_type;
 };
 
 export const ALL_LANG_SCRIPT_LIST = Array.from(new Set([...LANG_LIST, ...SCRIPT_LIST])).filter(

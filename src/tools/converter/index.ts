85ecf58b5cc7e633ab88f50f0cd28671411dc56a
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-01-10 13:24:30 +0530

    add

---
+++ src/tools/converter/index.ts
@@ -0,0 +1,29 @@
+import { transliterate, type ScriptLangType, type TransliterationOptions } from 'lipilekhika';
+
+/**
+ * Custom Options used for Transliteration in this project
+ */
+export const CUSTOM_TRANS_OPTIONS = {
+  'all_to_normal:remove_virAma_and_double_virAma': true,
+  'all_to_normal:replace_avagraha_with_a': true,
+  'all_to_normal:replace_pancham_varga_varna_with_n': true,
+  'all_to_sinhala:use_conjunct_enabling_halant': true
+} satisfies TransliterationOptions;
+
+/**
+ * Custom `transliterate` function which applies the `CUSTOM_TRANS_OPTIONS`
+ */
+export async function transliterate_custom<T extends string | string[]>(
+  text: T,
+  from: ScriptLangType,
+  to: ScriptLangType,
+  trans_options?: TransliterationOptions
+): Promise<T extends string ? string : string[]> {
+  if (typeof text === 'string') {
+    return (await transliterate(text, from, to, trans_options)) as any;
+  }
+  const results = await Promise.all(
+    (text as string[]).map((v) => transliterate(v, from, to, trans_options))
+  );
+  return results as any;
+}

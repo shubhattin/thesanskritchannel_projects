8149043a6dbd09970b060640c7a0d8a55ad63d38
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-01-31 08:22:37 +0530

    index+schema done

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,3 @@
-import { relations } from 'drizzle-orm';
 import {
   pgTable,
   text,
@@ -9,6 +8,7 @@ import {
   index,
   jsonb
 } from 'drizzle-orm/pg-core';
+// import { relations } from 'drizzle-orm';
 
 export const texts = pgTable(
   'texts',

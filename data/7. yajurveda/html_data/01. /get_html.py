0c6c63a1d8eb5da56585f8418ebd8a7e02bc3434
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-02-06 16:39:26 +0530

    add links structure

---
+++ data/7. yajurveda/html_data/01. /get_html.py
@@ -0,0 +1,6 @@
+RANGE_LINKS = {
+    "1-10": "https://vedicheritage.gov.in/samhitas/yajurveda/vajasneyi-madhyandina-samhita/samhita-patha-0-10-adhyaya-01/",
+    "11-20": "https://vedicheritage.gov.in/samhitas/yajurveda/vajasneyi-madhyandina-samhita/samhita-patha-11-20-adhyaya-01-2/",
+    "21-30": "https://vedicheritage.gov.in/samhitas/yajurveda/vajasneyi-madhyandina-samhita/samhita-patha-21-30-adhyaya-21/",
+    "31-40": "https://vedicheritage.gov.in/samhitas/yajurveda/vajasneyi-madhyandina-samhita/samhita-patha31-40-adhyaya-31/",
+}

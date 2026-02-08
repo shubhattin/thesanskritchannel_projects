a87131b960ecf3cff2244bcae023d533c9b16496
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-02-08 23:20:09 +0530

    init change

--- src/state/data_types.ts
+++ src/state/data_types.ts
@@ -1,52 +1,78 @@
 import { z } from 'zod';
 
-export const type_1_map_schema = z.object({
+const recursive_list_type = z.object({
   name_dev: z.string().describe('Name in Devanagari'),
-  name_nor: z.string().describe('Name in English'),
+  name_nor: z.string().describe('Name in English(normalized)'),
+  // Every text map starts with a top level where `name_dev` is actually the name of the text
   pos: z.int().describe('Position starting from 1'),
-  shloka_count: z.int(),
-  shloka_count_expected: z.int().optional().describe('Expected Shloka Count'),
-  total: z.int()
+  level_name: z.string().describe('Level/ListType Name'),
+  // ^ this level name is "Text" specific, like Sarga, Kanda, Chapter, Mandala, Sukta, etc.
+  get list() {
+    return recursive_list_type.array();
+  },
+  info: z.discriminatedUnion('type', [
+    z.object({
+      type: z.literal('shloka'), // this marks the lowest level and end of the recursive structure
+      // shloka, mantra and others are used as the same thing to mean
+      shloka_count: z.int(),
+      shloka_count_expected: z.int().optional().describe('Expected Shloka Count')
+    }),
+    z.object({
+      type: z.literal('list'),
+      list_count: z.int(),
+      list_count_expected: z.int().optional().describe('Expected List Count')
+    })
+  ])
 });
-export type type_1_map_type = z.infer<typeof type_1_map_schema>;
+export type recursive_list_type = z.infer<typeof recursive_list_type>;
 
-export const type_2_map_schema = type_1_map_schema.array();
-export type tyoe_2_map_type = z.infer<typeof type_2_map_schema>;
+// export const type_1_map_schema = z.object({
+//   name_dev: z.string().describe('Name in Devanagari'),
+//   name_nor: z.string().describe('Name in English'),
+//   pos: z.int().describe('Position starting from 1'),
+//   shloka_count: z.int(),
+//   shloka_count_expected: z.int().optional().describe('Expected Shloka Count'),
+//   total: z.int()
+// });
+// export type type_1_map_type = z.infer<typeof type_1_map_schema>;
 
-export const type_3_map_schema = z
-  .object({
-    name_dev: z.string().describe('Name in Devanagari'),
-    name_nor: z.string().describe('Name in English'),
-    pos: z.int().describe('Position Starting from 1'),
-    list_count: z.int(),
-    list: type_2_map_schema,
-    list_count_expected: z.int().optional().describe('Expected List Count')
-  })
-  .array();
-export type type_3_map_type = z.infer<typeof type_3_map_schema>;
+// export const type_2_map_schema = type_1_map_schema.array();
+// export type tyoe_2_map_type = z.infer<typeof type_2_map_schema>;
 
-export const type_4_map_schema = z
-  .object({
-    name_dev: z.string().describe('Name in Devanagari'),
-    name_nor: z.string().describe('Name in English'),
-    pos: z.int().describe('Position Starting from 1'),
-    list_count: z.int(),
-    list: type_3_map_schema,
-    list_count_expected: z.int().optional().describe('Expected List Count')
-  })
-  .array();
-export type type_4_map_type = z.infer<typeof type_4_map_schema>;
+// export const type_3_map_schema = z
+//   .object({
+//     name_dev: z.string().describe('Name in Devanagari'),
+//     name_nor: z.string().describe('Name in English'),
+//     pos: z.int().describe('Position Starting from 1'),
+//     list_count: z.int(),
+//     list: type_2_map_schema,
+//     list_count_expected: z.int().optional().describe('Expected List Count')
+//   })
+//   .array();
+// export type type_3_map_type = z.infer<typeof type_3_map_schema>;
 
-export const type_5_map_schema = z
-  .object({
-    name_dev: z.string().describe('Name in Devanagari'),
-    name_nor: z.string().describe('Name in English'),
-    pos: z.int().describe('Position Starting from 1'),
-    list: type_4_map_schema,
-    list_count_expected: z.int().optional().describe('Expected List Count')
-  })
-  .array();
-export type type_5_map_type = z.infer<typeof type_5_map_schema>;
+// export const type_4_map_schema = z
+//   .object({
+//     name_dev: z.string().describe('Name in Devanagari'),
+//     name_nor: z.string().describe('Name in English'),
+//     pos: z.int().describe('Position Starting from 1'),
+//     list_count: z.int(),
+//     list: type_3_map_schema,
+//     list_count_expected: z.int().optional().describe('Expected List Count')
+//   })
+//   .array();
+// export type type_4_map_type = z.infer<typeof type_4_map_schema>;
+
+// export const type_5_map_schema = z
+//   .object({
+//     name_dev: z.string().describe('Name in Devanagari'),
+//     name_nor: z.string().describe('Name in English'),
+//     pos: z.int().describe('Position Starting from 1'),
+//     list: type_4_map_schema,
+//     list_count_expected: z.int().optional().describe('Expected List Count')
+//   })
+//   .array();
+// export type type_5_map_type = z.infer<typeof type_5_map_schema>;
 
 export const shloka_schema = z.object({
   text: z.string().describe('Shloka Text'),
--- src/state/project_list.ts
+++ src/state/project_list.ts
@@ -1,11 +1,5 @@
 import { z } from 'zod';
-import type {
-  tyoe_2_map_type,
-  type_1_map_type,
-  type_3_map_type,
-  type_4_map_type,
-  type_5_map_type
-} from './data_types';
+import type { recursive_list_type } from './data_types';
 
 const PROJECT_KEYS = [
   'ramayanam',
@@ -13,8 +7,7 @@ const PROJECT_KEYS = [
   'narayaneeyam',
   'shivatandavastotram',
   'saundaryalahari',
-  'rgveda',
-  'yajurveda'
+  'rgveda'
 ] as const;
 export const project_keys_enum_schema = z.enum(PROJECT_KEYS);
 export type project_keys_type = z.infer<typeof project_keys_enum_schema>;
@@ -25,10 +18,11 @@ type project_type = {
   name_dev: string;
   description?: string;
   key: project_keys_type;
+  get_map?: () => Promise<recursive_list_type>;
 };
 
 // ALWAYS BE CAREFUL BEFORE CHANGING THIS LIST
-// AS CHNAGE IN PRE-EXISTING PROJECT IDS WOULD CAUSE DATA MISMATCHs
+// AS CHANGE IN PRE-EXISTING PROJECT IDS WOULD CAUSE DATA MISMATCHs
 
 export const PROJECT_LIST: project_type[] = [
   {
@@ -53,25 +47,25 @@ export const PROJECT_LIST: project_type[] = [
     id: 4,
     name: 'Shiva Tandava Stotra',
     name_dev: 'शिवताण्डवस्तोत्रम्',
-    key: 'shivatandavastotram'
+    key: 'shivatandavastotram',
+    get_map: async () =>
+      (await import('@data/4. shivatandavastotram/shivatandavastotram_map.json'))
+        .default as recursive_list_type
   },
   {
     id: 5,
     name: 'Saundarya Lahari',
     name_dev: 'सौन्दर्यलहरी',
-    key: 'saundaryalahari'
+    key: 'saundaryalahari',
+    get_map: async () =>
+      (await import('@data/5. saundaryalahari/saundaryalahari_map.json'))
+        .default as recursive_list_type
   },
   {
     id: 6,
     name: 'Rgveda',
     name_dev: 'ऋग्वेद',
     key: 'rgveda'
-  },
-  {
-    id: 7,
-    name: 'Yajurveda',
-    name_dev: 'यजुर्वेद',
-    key: 'yajurveda'
   }
 ];
 
@@ -83,100 +77,99 @@ export const get_project_from_key = (key: project_keys_type) => {
   return PROJECT_LIST[PROJECT_KEYS.indexOf(key)];
 };
 
-export type extendted_map_type =
-  | {
-      levels: 1;
-      map_info: () => Promise<type_1_map_type>;
-    }
-  | {
-      levels: 2;
-      map_info: () => Promise<tyoe_2_map_type>;
-    }
-  | {
-      levels: 3;
-      map_info: () => Promise<type_3_map_type>;
-    }
-  | {
-      levels: 5;
-      map_info: () => Promise<type_5_map_type>;
-    };
-
-export const get_map_type = <T extends extendted_map_type['levels']>(
-  map_info: Awaited<ReturnType<extendted_map_type['map_info']>>,
-  levels: T
-): T extends 1
-  ? type_1_map_type
-  : T extends 2
-    ? tyoe_2_map_type
-    : T extends 3
-      ? type_3_map_type
-      : T extends 4
-        ? type_4_map_type
-        : T extends 5
-          ? type_5_map_type
-          : never => {
-  return map_info as any;
-};
-type project_info_type = {
-  [K in project_keys_type]: {
-    // key: K;
-    /** The project level here also includes shloka */
-    level_names: string[];
-  } & extendted_map_type;
-};
-
-export const PROJECT_INFO: project_info_type = {
-  ramayanam: {
-    levels: 3,
-    level_names: ['Shloka', 'Sarga', 'Kanda'],
-    map_info: async () => (await import('@data/1. ramayanam/ramayanam_map.json')).default
-  },
-  bhagavadgita: {
-    levels: 2,
-    level_names: ['Shloka', 'Chapter'],
-    map_info: async () => (await import('@data/2. bhagavadgita/bhagavadgita_map.json')).default
-  },
-  narayaneeyam: {
-    levels: 2,
-    level_names: ['Shloka', 'Dashaka'],
-    map_info: async () => (await import('@data/3. narayaneeyam/narayaneeyam_map.json')).default
-  },
-  shivatandavastotram: {
-    levels: 1,
-    level_names: ['Shloka'],
-    map_info: async () =>
-      (await import('@data/4. shivatandavastotram/shivatandavastotram_map.json')).default
-  },
-  saundaryalahari: {
-    levels: 1,
-    level_names: ['Shloka'],
-    map_info: async () =>
-      (await import('@data/5. saundaryalahari/saundaryalahari_map.json')).default
-  },
-  rgveda: {
-    levels: 5,
-    level_names: ['Mantra', 'Sukta', 'Mandala', 'Bhaga', 'Shakha'],
-    map_info: async () => (await import('@data/6. rgveda/rgveda_map.json')).default
-  },
-  yajurveda: {
-    levels: 5,
-    level_names: ['Mantra', 'Adhyaya', 'Anuvaka', 'Bhaga', 'Shakha'],
-    map_info: async () => (await import('@data/7. yajurveda/yajurveda_map.json')).default
-  }
-};
-
-export const get_project_info_from_key = (key: project_keys_type) => {
-  return { ...PROJECT_INFO[key], key };
-};
-
-export const get_project_info_from_id = (id: number) => {
-  const project = PROJECT_LIST[id - 1];
-  return get_project_info_from_key(project.key);
-};
-
 export const get_path_params = (
   selected_text_levels: (number | null)[],
   project_levels: number
 ) => {
   return selected_text_levels.slice(0, project_levels - 1).reverse() as number[];
 };
+
+// export type extendted_map_type =
+//   | {
+//       levels: 1;
+//       map_info: () => Promise<type_1_map_type>;
+//     }
+//   | {
+//       levels: 2;
+//       map_info: () => Promise<tyoe_2_map_type>;
+//     }
+//   | {
+//       levels: 3;
+//       map_info: () => Promise<type_3_map_type>;
+//     }
+//   | {
+//       levels: 4;
+//       map_info: () => Promise<type_4_map_type>;
+//     }
+//   | {
+//       levels: 5;
+//       map_info: () => Promise<type_5_map_type>;
+//     };
+
+// export const get_map_type = <T extends extendted_map_type['levels']>(
+//   map_info: Awaited<ReturnType<extendted_map_type['map_info']>>,
+//   levels: T
+// ): T extends 1
+//   ? type_1_map_type
+//   : T extends 2
+//     ? tyoe_2_map_type
+//     : T extends 3
+//       ? type_3_map_type
+//       : T extends 4
+//         ? type_4_map_type
+//         : T extends 5
+//           ? type_5_map_type
+//           : never => {
+//   return map_info as any;
+// };
+// type project_info_type = {
+//   [K in project_keys_type]: {
+//     // key: K;
+//     /** The project level here also includes shloka */
+//     level_names: string[];
+//   } & extendted_map_type;
+// };
+
+// export const PROJECT_INFO: project_info_type = {
+//   ramayanam: {
+//     levels: 3,
+//     level_names: ['Shloka', 'Sarga', 'Kanda'],
+//     map_info: async () => (await import('@data/1. ramayanam/ramayanam_map.json')).default
+//   },
+//   bhagavadgita: {
+//     levels: 2,
+//     level_names: ['Shloka', 'Chapter'],
+//     map_info: async () => (await import('@data/2. bhagavadgita/bhagavadgita_map.json')).default
+//   },
+//   narayaneeyam: {
+//     levels: 2,
+//     level_names: ['Shloka', 'Dashaka'],
+//     map_info: async () => (await import('@data/3. narayaneeyam/narayaneeyam_map.json')).default
+//   },
+//   shivatandavastotram: {
+//     levels: 1,
+//     level_names: ['Shloka'],
+//     map_info: async () =>
+//       (await import('@data/4. shivatandavastotram/shivatandavastotram_map.json')).default
+//   },
+//   saundaryalahari: {
+//     levels: 1,
+//     level_names: ['Shloka'],
+//     map_info: async () =>
+//       (await import('@data/5. saundaryalahari/saundaryalahari_map.json')).default
+//   },
+//   rgveda: {
+//     levels: 5,
+//     level_names: ['Mantra', 'Sukta', 'Mandala', 'Bhaga', 'Shakha'],
+//     map_info: async () => (await import('@data/6. rgveda/rgveda_map.json')).default
+//   }
+// };
+
+// export const get_project_info_from_key = (key: project_keys_type) => {
+//   return { ...PROJECT_INFO[key], key };
+// };
+//
+// export const get_project_info_from_id = (id: number) => {
+//   const project = PROJECT_LIST[id - 1];
+//   return get_project_info_from_key(project.key);
+// };

1aa6ca07e789aab684f972a3fdc411b86f962a41
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-03-26 14:08:18 +0530

    bun runtime

--- site/vitest.config.ts
+++ site/vitest.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@data': '../data'
+      '$app': fileURLToPath(new URL('../app/src', import.meta.url)),
+      '@data': fileURLToPath(new URL('../data', import.meta.url))
     }
   },
   test: {

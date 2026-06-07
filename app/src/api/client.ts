bac6d938d8f8e57e8a4674ccbfd4441a10a0ae2a
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-06-07 20:36:13 +0530

    a

--- app/src/api/client.ts
+++ app/src/api/client.ts
@@ -1,8 +1,7 @@
 import type { Router } from '~/api/trpc_router';
-import transformer from './transformer';
-
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import { createTRPCContext } from 'trpc-tanstack-svelte-query';
+import transformer from './transformer';
 
 export const client = createTRPCClient<Router>({
   links: [
@@ -14,15 +13,3 @@ export const client = createTRPCClient<Router>({
 });
 
 export const { useTRPC, useTRPCClient } = createTRPCContext<Router>();
-
-// const client_options = {
-//   links: [
-//     httpBatchLink({
-//       url: '/trpc'
-//     })
-//   ],
-//   transformer
-// };
-
-// export const client = createTRPCClient<Router>(client_options);
-// export const client_q = createTRPCSvelte<Router>(client_options);
